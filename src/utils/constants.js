6981d55e221719dcd167e27049b439d36c2dc83c
Gia Duc <giaduc19042004@gmail.com>
2025-05-26 11:28:51 +0700

    update cors not allow localhost

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,8 +1,5 @@
 export const WHITELIST_DOMAINS = [
-  'http://localhost:5173'
-]
+  // 'http://localhost:5173'
 
-// export const BOARD_TYPES = {
-//   PUBLIC: 'public',
-//   PRIVATE: 'private'
-// }
+  // sau khi deploy backend lên production thì không cần localhost nữa vì ở file config/cors đã luôn cho phép môi trường dev
+]
