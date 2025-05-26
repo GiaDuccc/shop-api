0d9c7c314f243da04cbc99e37089f33952a20b03
Gia Duc <giaduc19042004@gmail.com>
2025-05-26 12:57:53 +0700

    fix build mode

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,5 @@
 export const WHITELIST_DOMAINS = [
   // 'http://localhost:5173'
-
+  'https://shop-web-three.vercel.app/'
   // sau khi deploy backend lên production thì không cần localhost nữa vì ở file config/cors đã luôn cho phép môi trường dev
 ]
