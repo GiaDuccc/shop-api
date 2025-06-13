2089a3e7d300a6512494e66a598e3e5313733d54
Gia Duc <giaduc19042004@gmail.com>
2025-06-13 15:19:08 +0700

    update domains

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,7 @@
 export const WHITELIST_DOMAINS = [
   // 'http://localhost:5173'
-  'https://shop-web-three.vercel.app'
+  'https://shop-web-three.vercel.app',
+  'https://www.giaduccc.works/',
+  'https://giaduccc.works'
   // sau khi deploy backend lên production thì không cần localhost nữa vì ở file config/cors đã luôn cho phép môi trường dev
 ]
